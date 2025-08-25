7943a32ed3f60fd232a5c0bc78e4eb5c59af1aa0
Anmol Sharma <anmolsharma0541@gmail.com>
2025-08-25 14:42:38 +0530

    Add files via upload

---
+++ samadhan/DAY1/Student.js
@@ -0,0 +1,13 @@
+const student = {
+  name: "Anmol",
+  age: 21,
+  course: "B.Tech Computer Science",
+  marks: 92
+};
+
+// Print student details
+console.log("Student Details:");
+console.log("Name: " + student.name);
+console.log("Age: " + student.age);
+console.log("Course: " + student.course);
+console.log("Marks: " + student.marks);
